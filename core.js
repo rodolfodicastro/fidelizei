4d422d55de9f1cef7ca33f630b2d7019053d53a2
philypeabreu <philype01@gmail.com>
2021-08-03 21:58:10 -0300

    modificações e parendizado da primeira aula.

---
+++ core.js
@@ -0,0 +1,6 @@
+var inputTelefone = document.querySelector("#telefone")
+var btnPesquisar = document.querySelector("#pesquisar") 
+
+btnPesquisar.addEventListener("click",function()
+{alert(inputTelefone.value)})
+
