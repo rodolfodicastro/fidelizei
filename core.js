b77c969f91f7dae87a09b5ad0b297a654e42cead
Rodolfo <rodolfodicastro@yahoo.com.br>
2021-08-17 22:18:28 -0300

    17/08

--- core.js
+++ core.js
@@ -1,6 +1,51 @@
+var mockData=[
+    {
+        id:8190001,
+        dataCompra: "00/11/0000",
+        pontos: 1
+    },
+    {
+        id:8190002,
+        dataCompra: "00/11/0002",
+        pontos: 1
+    },
+    {
+        id:8190003,
+        dataCompra: "00/11/0003",
+        pontos: 1
+    }
+]
+
+
 var inputTelefone = document.querySelector("#telefone")
 var btnPesquisar = document.querySelector("#pesquisar") 
+var telefone=""
 
-btnPesquisar.addEventListener("click",function()
-{alert(inputTelefone.value)})
+function pesquisarNaLista(item){
+console.log(item)
+    if ((item.id+"")==telefone){
+
+        return true
+    }
+    else {
+        return false
+    }
+
+
+}
+
+function pesquisar(){
+    
+    telefone=inputTelefone.value
+    if(telefone==""){
+        alert("Digite seu Telefone") 
+        return
+    }
+var dadosEncontrados = mockData.filter(pesquisarNaLista)
+console.log(dadosEncontrados)    
+}
+
+
+
+btnPesquisar.addEventListener("click",pesquisar)
 
