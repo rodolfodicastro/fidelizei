5e6c8eec9ab65245bc2f6b9616747f79d6981a36
Rodolfo Dicastro <rodolfodicastro@yahoo.com.br>
2021-09-21 22:52:00 -0300

    Metodo

--- core.js
+++ core.js
@@ -1,16 +1,16 @@
 var mockData=[
     {
-        id:8190001,
+        id:10,
         dataCompra: "05/11/2021",
         pontos: 1
     },
     {
-        id:8190002,
+        id:20,
         dataCompra: "10/11/2021",
         pontos: 1
     },
     {
-        id:8190003,
+        id:30,
         dataCompra: "14/11/2021",
         pontos: 1
     }
@@ -21,7 +21,15 @@ var inputTelefone = document.querySelector("#telefone")
 var btnPesquisar = document.querySelector("#pesquisar") 
 var table = document.querySelector("#tabelaDeCompras") 
 var telefone=""
-var template="<tr> <td>@dataCompra</td> <td>@pontos</td> </tr>"
+var template
+var minhaTR=document.createElement("tr")
+var dataTD=document.createElement("td")
+dataTD.id="DataDaCompra"
+var pontosTD=document.createElement("td")
+pontosTD.id="Pontos"
+minhaTR.appendChild(dataTD)
+minhaTR.appendChild(pontosTD)
+
 
 function pesquisar(){
     
@@ -32,16 +40,18 @@ function pesquisar(){
     }
 
     var tomanhoDaMinhaLista = mockData.length
-    var oCaraQueFoiEncontrado
+
     for(interator = 0; tomanhoDaMinhaLista > interator; interator = interator+1){
        
         var compraAtual = mockData[interator]
         if(compraAtual.id ==telefone){
-            oCaraQueFoiEncontrado = compraAtual
+            dataTD.innerText=compraAtual.dataCompra
+            pontosTD.innerText=compraAtual.pontos
+            table.appendChild(minhaTR)
         }
     }
 
-    console.log(oCaraQueFoiEncontrado)
+
 
 
 }
