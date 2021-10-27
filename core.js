3f9116bf79568f912673c4af0df4a63664c5bf21
Rodolfo <rodolfodicastro@yahoo.com.br>
2021-10-26 23:02:05 -0300

    ijhu

--- core.js
+++ core.js
@@ -1,16 +1,16 @@
-var mockData=[
+var mockData = [
     {
-        id:10,
+        id: 10,
         dataCompra: "05/11/2021",
         pontos: 1
     },
     {
-        id:20,
+        id: 20,
         dataCompra: "10/11/2021",
         pontos: 1
     },
     {
-        id:30,
+        id: 30,
         dataCompra: "14/11/2021",
         pontos: 1
     }
@@ -18,46 +18,46 @@ var mockData=[
 
 
 var inputTelefone = document.querySelector("#telefone")
-var btnPesquisar = document.querySelector("#pesquisar") 
-var btnIncluir = document.querySelector("#incluir") 
-var table = document.querySelector("#tabelaDeCompras") 
-var telefone=""
+var btnPesquisar = document.querySelector("#pesquisar")
+var btnIncluir = document.querySelector("#incluir")
+var table = document.querySelector("#tabelaDeCompras")
+var telefone = ""
 var template
-var minhaTR=document.createElement("tr")
-var dataTD=document.createElement("td")
-dataTD.id="DataDaCompra"
-var pontosTD=document.createElement("td")
-pontosTD.id="Pontos"
+var minhaTR = document.createElement("tr")
+var dataTD = document.createElement("td")
+dataTD.id = "DataDaCompra"
+var pontosTD = document.createElement("td")
+pontosTD.id = "Pontos"
 minhaTR.appendChild(dataTD)
 minhaTR.appendChild(pontosTD)
 
 
-function pesquisar(){
-    
-    telefone=inputTelefone.value
-    if(telefone==""){
-        alert("Digite seu Telefone") 
- 
+function pesquisar() {
+
+    telefone = inputTelefone.value
+    if (telefone == "") {
+        alert("Digite seu Telefone")
+
         return
     }
 
-fetch('http://localhost:3000/compras/'+telefone)
-  .then((response) => {
-    return response.json();
-  })
-  .then((compraAtual) => {
-      if (compraAtual.dataCompra != undefined){
-        dataTD.innerText=compraAtual.dataCompra
-        pontosTD.innerText=compraAtual.pontos
-        table.appendChild(minhaTR);
-      }
-  });
+    fetch('http://localhost:3000/compras/' + telefone)
+        .then((response) => {
+            return response.json();
+        })
+        .then((compraAtual) => {
+            if (compraAtual.dataCompra != undefined) {
+                dataTD.innerText = compraAtual.dataCompra
+                pontosTD.innerText = compraAtual.pontos
+                table.appendChild(minhaTR);
+            }
+        });
 
 
     // var tomanhoDaMinhaLista = mockData.length
 
     // for(interator = 0; tomanhoDaMinhaLista > interator; interator = interator+1){
-       
+
     //     var compraAtual = mockData[interator]
     //     if(compraAtual.id ==telefone){
     //         dataTD.innerText=compraAtual.dataCompra
@@ -70,44 +70,32 @@ fetch('http://localhost:3000/compras/'+telefone)
 
 }
 
-function incluir(){
-    
-    telefone=inputTelefone.value
-    if(telefone==""){
-        alert("Digite seu Telefone") 
+function incluir() {
+
+    telefone = inputTelefone.value
+    if (telefone == "") {
+        alert("Digite seu Telefone")
         return
     }
-    var tomanhoDaMinhaLista = mockData.length
-    for(interator = 0; tomanhoDaMinhaLista > interator; interator = interator+1){
-       
-        var compraAtual = mockData[interator]
-        if(compraAtual.id ==telefone){
-        compraAtual.pontos=compraAtual.pontos+1
-        inputTelefone.value=""
-        alert("Adicionado com Sucesso") 
-        return
-        }
-    }
+    fetch('http://localhost:3000/compras/', {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        method: 'POST', 
+        body: JSON.stringify({ telefone: telefone }) 
+    }).then((res) => {
 
+            alert(`Incluido com Sucesso! :)))`)
+        });
 
-    var dataAtual= new Date()
-    var dataAtualFormatada = dataAtual.getDate()+"/"+(dataAtual.getMonth()+1)+"/"+dataAtual.getFullYear()
-    var compraAtual={
-        id:telefone,
-        dataCompra: dataAtualFormatada,
-        pontos: 1
-    }
-
-mockData.push(compraAtual)
-inputTelefone.value=""
-alert("Incluido com Sucesso")
 }
 
 
-btnPesquisar.addEventListener("click",pesquisar)
+btnPesquisar.addEventListener("click", pesquisar)
 
 
-btnIncluir.addEventListener("click",incluir)
+btnIncluir.addEventListener("click", incluir)
 
 // btnExcluir.addEventListener("click",excluir)
 
